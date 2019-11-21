6710f767a25c6567cfed5b86f29996e3968abd11
Abhi Bhaiyya <jioatm1990@gmail.com>
2019-11-21 10:57:45 +0530

    2nd commit from master

--- 1.py
+++ 1.py
@@ -2,3 +2,10 @@ print("Hello this is Abhishek,Pranit and Sharmila's file")
 print("Welcome ChachaSir to the group!!!!")
 print("Sir you're not my CR")
 print("Mrinal Gade you're my CR")
+
+print("Message from dbda branch1")
+print("DBDA Branch1")
+
+print("This is message from DBDA2")
+print("Hello from DBDA Branch2")
+

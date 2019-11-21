dc4f09370659d679f2ada55f4e2b3e860fdece2d
Abhi Bhaiyya <jioatm1990@gmail.com>
2019-11-21 11:12:34 +0530

    DBDA6

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Test1 {
+
+<<<<<<< HEAD
+	public static void main(String[] args) 
+	{
+		System.out.println("My first simple Java code on Github");
+		System.out.println("My name is Pranit");
+	}
+
+}
+=======
+        public static void main(String[] args) 
+        {
+		System.out.println("dbda branch");
+                System.out.println("My first simple Java code on Github");
+                System.out.println("My name is Abhishek");
+        }
+
+}
+
+>>>>>>> dbda
